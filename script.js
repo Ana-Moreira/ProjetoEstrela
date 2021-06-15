2581546eea37ef39f4d6c180a232ed4a66b72a2c
Ana-moreira <anabrtcc88@gmail.com>
2021-06-15 16:56:52 -0300

    Finalizado

--- script.js
+++ script.js
@@ -3,19 +3,18 @@ const imagem2 = document.querySelector('#img2');
 const imagem3 = document.querySelector('#img3');
 const imagem4 = document.querySelector('#img4');
 const button = document.querySelector('button')
+let p1 = document.getElementById("personagem1");
+let p2 = document.getElementById("personagem2");
+let p3 = document.getElementById("personagem3");
+let p4 = document.getElementById("personagem4");
 
-let div1 = document.getElementById("personagem1");
-let div2 = document.getElementById("personagem2");
-let div3 = document.getElementById("personagem3");
-let div4 = document.getElementById("personagem4");
-
-gerarValorAleatorio = () => {
+valorAleatorio = () => {
     return Math.floor(Math.random() * 671);
 }
 
-pegarPersonagem1 = () => {
-    let valorAleatorio = gerarValorAleatorio();
-    return fetch(`https://rickandmortyapi.com/api/character/${valorAleatorio}`,{        
+personagem1 = () => {
+    let aleatorio = valorAleatorio();
+    return fetch(`https://rickandmortyapi.com/api/character/${aleatorio}`, {
         method: 'GET',
         headers: {
             Accept: 'application/json',
@@ -24,13 +23,13 @@ pegarPersonagem1 = () => {
     }).then((response) => response.json()).then((data) => {
         imagem1.src = data.image;
         imagem1.alt = data.name;
-        div1.innerText = imagem1.alt;    
-    });        
-} 
+        p1.innerText = imagem1.alt;
+    });
+}
 
-pegarPersonagem2 = () => {
-    let valorAleatorio = gerarValorAleatorio();
-    return fetch(`https://rickandmortyapi.com/api/character/${valorAleatorio}`,{        
+personagem2 = () => {
+    let aleatorio = valorAleatorio();
+    return fetch(`https://rickandmortyapi.com/api/character/${aleatorio}`, {
         method: 'GET',
         headers: {
             Accept: 'application/json',
@@ -39,13 +38,13 @@ pegarPersonagem2 = () => {
     }).then((response) => response.json()).then((data) => {
         imagem2.src = data.image;
         imagem2.alt = data.name;
-        div2.innerText = imagem2.alt;      
-    });        
+        p2.innerText = imagem2.alt;
+    });
 }
 
-pegarPersonagem3 = () => {
-    let valorAleatorio = gerarValorAleatorio();
-    return fetch(`https://rickandmortyapi.com/api/character/${valorAleatorio}`,{        
+personagem3 = () => {
+    let aleatorio = valorAleatorio();
+    return fetch(`https://rickandmortyapi.com/api/character/${aleatorio}`, {
         method: 'GET',
         headers: {
             Accept: 'application/json',
@@ -54,13 +53,13 @@ pegarPersonagem3 = () => {
     }).then((response) => response.json()).then((data) => {
         imagem3.src = data.image;
         imagem3.alt = data.name;
-        div3.innerText = imagem3.alt;     
-    });        
+        p3.innerText = imagem3.alt;
+    });
 }
 
-pegarPersonagem4 = () => {
-    let valorAleatorio = gerarValorAleatorio();
-    return fetch(`https://rickandmortyapi.com/api/character/${valorAleatorio}`,{        
+personagem4 = () => {
+    let aleatorio = valorAleatorio();
+    return fetch(`https://rickandmortyapi.com/api/character/${aleatorio}`, {
         method: 'GET',
         headers: {
             Accept: 'application/json',
@@ -69,15 +68,15 @@ pegarPersonagem4 = () => {
     }).then((response) => response.json()).then((data) => {
         imagem4.src = data.image;
         imagem4.alt = data.name;
-        div4.innerText = imagem4.alt;    
-    });        
+        p4.innerText = imagem4.alt;
+    });
 }
 
 iniciar = () => {
-    pegarPersonagem1();
-    pegarPersonagem2();
-    pegarPersonagem3();
-    pegarPersonagem4();     
+    personagem1();
+    personagem2();
+    personagem3();
+    personagem4();
 }
 
 button.onclick = iniciar;
